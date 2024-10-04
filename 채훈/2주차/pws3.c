8bd66decc049f6e9b52f2967ed8d5da28e50aace
leechaehon <chaehon123@gmail.com>
2024-10-04 16:55:39 +0900

    a

--- 채훈/2주차/pws3.c
+++ 채훈/2주차/pws3.c
@@ -1,1 +1,20 @@
-ddd
+#include <stdio.h>
+
+#include <math.h>
+int isprime(int n);
+int main(void)
+{
+	int a;
+	scanf("%d", &a);
+	if (isprime(a)) printf("a prime\n");
+	else printf("not a prime\n");
+	return 0;
+}
+int isprime(int n)
+{
+	int ni;
+	if (n <= 1) return 0;
+	for (ni = 2; ni < sqrt(n) +1; ni = ni + 1)
+		if (!(n % ni)) return 0;
+	return (1);
+}
